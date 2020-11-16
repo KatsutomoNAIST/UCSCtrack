1595e241c8746c836d498e7e0cac6070457f1c65
KatsutomoNAIST <57120698+KatsutomoNAIST@users.noreply.github.com>
2020-11-16 14:58:06 +0900

    Update converting_BEDtoBB.sh

--- converting_BEDtoBB.sh
+++ converting_BEDtoBB.sh
@@ -16,4 +16,5 @@ sort -k1,1 -k2,2n readingframe.bed > readingframe.sorted.bed
 ##convert bed to bigbed
 bedToBigBed readingframe.sorted.bed hg38.chrom.sizes readingframe.bb
 
-## track type=bigBed name="ReadingFrame" description="Reading frame of exon" itemRgb="On" bigDataUrl=https://drive.google.com/file/d/1-2qfpwHKVo5FC_8Yx4IDf3h6CSg98Jsb/view?usp=sharing
+## track type=bigBed name="ReadingFrame" description="Reading frame of exon" itemRgb="On" bigDataUrl=https://github.com/KatsutomoNAIST/UCSCtrack/raw/master/readingframe.bb
+
